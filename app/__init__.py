b80076b7cb7dbed966dc8f7710c033ea47fe0467
egbakou <egbakoukodjolaurent@gmail.com>
2020-04-22 15:33:44 +0000

    app _init_ updated .

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,11 @@
 """
-''
+Corona Virus Tracker API
+~~~~~~~~~~~~~~~~~~~~~~~~
+API for tracking the global coronavirus (COVID-19, SARS-CoV-2) outbreak in Togo.
 """
+import logging
+
+# See PEP396.
+__version__ = "1.0.0"
+
+logging.basicConfig(level=logging.INFO)
